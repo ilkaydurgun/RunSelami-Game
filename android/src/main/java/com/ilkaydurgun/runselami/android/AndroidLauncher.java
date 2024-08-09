4e33a52b170c6654100d403ee5e007854eef5410
Ilkay Durgun <ilkydrgun99@gmail.com>
2024-08-09 11:25:17 +0300

    RunSelami Added

---
+++ android/src/main/java/com/ilkaydurgun/runselami/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.ilkaydurgun.runselami.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.ilkaydurgun.runselami.RunSelami;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new RunSelami(), configuration);
+    }
+}
